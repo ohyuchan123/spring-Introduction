74d1a2a5f8fad59a06a0c3c686a43f387cd64a80
ohyuchan123 <oyuchan50@gmail.com>
2023-01-12 00:02:02 +0900

    API

--- src/main/java/hello/demo/controller/HelloController.java
+++ src/main/java/hello/demo/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -18,5 +19,29 @@ public class HelloController {
     public String helloMvc(@RequestParam("name")String name,Model model){
         model.addAttribute("name",name);
         return "hello-templates";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody //http body에 문자 내용을 직접 반환
+    public String helloString(@RequestParam("name")String name){
+        return "hello"+name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name")String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
