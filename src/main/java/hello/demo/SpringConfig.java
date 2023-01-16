604f344b6d6c4775847c475041ce3907aac75203
ohyuchan123 <oyuchan50@gmail.com>
2023-01-16 23:18:11 +0900

    자바 코드로 스프링 빈 등록하기

---
+++ src/main/java/hello/demo/SpringConfig.java
@@ -0,0 +1,22 @@
+package hello.demo;
+
+import hello.demo.repository.MemberRepository;
+import hello.demo.repository.MemoryMemberRepository;
+import hello.demo.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+
+}
